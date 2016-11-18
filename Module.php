389c4891c70566d21caebc8796ec30bb4155f732
xutl <79109681@qq.com>
2016-11-18 17:44:34 +0800

    搜索

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\search;
+
+use Yii;
+
+
+/**
+ * This is the main module class for the yii2-user.
+ */
+class Module extends \yii\base\Module
+{
+
+}
